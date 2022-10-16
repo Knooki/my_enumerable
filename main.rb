687b14f646aa7fa129f60889ec40e88dfbae49d5
knooki <knukknakiknak@gmail.com>
2022-10-16 20:00:44 +0300

    all functions

--- main.rb
+++ main.rb
@@ -13,85 +13,162 @@ puts "\n"
 
 # all? any? include? none? each map size count length select find find_all find_index reject max min
 
+# puts 'all? array1'
+# p array1.all?(Numeric)
+# p array1.all? { |x| x > 3 }
+# p array1.all?(5) { |x| x > 3 }
+
 # puts 'my_all? array1'
-# puts '(Numeric) '
 # p array1.my_all?(Numeric)
-# puts '{ |x| x > 3 }'
 # p array1.my_all? { |x| x > 3 }
+# p array1.my_all?(5) { |x| x > 3 }
 # puts "\n"
 
-# puts 'my_any? array2'
-# puts 'are 5?'
-# p array1.my_any?(String)
-# puts 'are bigger than 6'
-# p array1.my_any? { |x| x > 6 }
+# puts 'any? array1'
+# p array1.any?(Numeric)
+# p array1.any? { |x| x > 3 }
+# p array1.any?(5) { |x| x > 3 }
+
+# puts 'my_any? array1'
+# p array1.my_any?(Numeric)
+# p array1.my_any? { |x| x > 3 }
+# p array1.my_any?(5) { |x| x > 3 }
 # puts "\n"
 
-# puts 'my_include? array1 in array2'
-# p array2.my_include?(array1)
-# puts "\n"
+# puts 'none? array1'
+# p array1.none?(Numeric)
+# p array1.none? { |x| x < 3 }
+# p array1.none?(5) { |x| x > 3 }
 
 # puts 'my_none? array1'
-# puts 'are 5?'
-# p array1.my_none?(5)
-# puts 'are bigger than 7'
-# p array1.my_none? { |n| n > 7 }
+# p array1.my_none?(Numeric)
+# p array1.my_none? { |x| x < 3 }
+# p array1.my_none?(5) { |x| x > 3 }
 # puts "\n"
 
-puts 'my_each array2'
-puts 'enum'
-p array2.my_each
-puts "\n{|x| x=10}"
-p array2.my_each { |item| item = 10 }
-puts "\n"
+# puts "each array2"
+# p array2.each
+# array2.each {|item| print "item", item}
+
+# puts 'my_each array2'
+# p array2.my_each
+# array2.my_each {|item| print "item", item}
+
+# puts 'my_include? 3 in array2'
+# p array2.my_include?(3)
+# puts "\n"
+
+# puts 'include? 3 in array2'
+# p array2.include?(3)
 
 # puts 'my_map array2'
 # puts 'set to 5'
-# p array2.my_map(5)
+# p array2.my_map! {|x| x = 5}
+# p array2
+# puts "\n"
+
+# puts 'my_map array2'
+# puts 'set to 5'
+# p array2.map! {|x| x = 6}
+# p array2
 # puts "\n"
 
 # puts 'my_size array2'
 # p array2.my_size
 # puts "\n"
 
-# puts 'my_length array1'
-# p array1.my_length
+# puts "size array2"
+# p array2.size
 # puts "\n"
 
+# puts 'count array2'
+# p array2.count
+# p array2.count(3)
+# p array2.count { |x| x > 4 }
+# p array2.count(3) { |x| x > 4 }
+
 # puts 'my_count array2'
-# puts 'count bigger than 4 starting with 3'
+# p array2.my_count
+# p array2.my_count(3)
+# p array2.my_count { |x| x > 4 }
 # p array2.my_count(3) { |x| x > 4 }
 # puts "\n"
 
-# puts 'my_select array1'
-# puts 'select where x<6'
-# p array1.my_select { |x| x < 6 }
+# puts "length array2"
+# p array2.length
 # puts "\n"
+
+# puts 'my_length array2'
+# p array2.my_length
+# puts "\n"
+
+# puts 'select array2'
+# p array2.select
+# p array2.select { |x| x < 6 }
+# puts "\n"
+
+# puts 'my_select array2'
+# p array2.my_select
+# p array2.my_select { |x| x < 6 }
+# puts "\n"
+
+# arg = Proc.new {"No matches found"}
+
+# puts 'find array2'
+# p array2.find(arg)
+# p array2.find(arg) { |i| i % 10 == 0 } #=> nil
 
 # puts 'my_find array2'
-# puts 'find first 6'
-# p array2.my_find(5)
+# p array2.my_find(arg)
+# p array2.my_find(arg) { |i| i % 10 == 0 } #=> nil
 # puts "\n"
 
+# puts 'find_all array2'
+# p array2.find_all
+# p array2.find_all { |x| x > 10 }
+
 # puts 'my_find_all array1'
-# puts 'find_all where x<6'
-# p array1.my_find_all { |x| x < 6 }
+# p array2.my_find_all
+# p array2.my_find_all { |x| x > 10 }
+# puts "\n"
+
+# puts "find_index array2"
+# p array2.find_index()
+# p array2.find_index(3)
+# p array2.find_index(3) {|x| x > 4}
+# p array2.find_index {|x| x > 4}
 # puts "\n"
 
 # puts 'my_find_index array1'
-# puts 'find first index where x<6'
-# p array1.my_find_index { |x| x < 6 }
+# p array2.my_find_index()
+# p array2.my_find_index(3)
+# p array2.my_find_index(3) {|x| x > 4}
+# p array2.my_find_index {|x| x > 4}
 # puts "\n"
 
-# puts 'my_reject array1'
-# puts 'reject where x<6'
-# p array1.my_reject { |x| x < 6 }
-# puts "\n"
+# puts 'reject array2'
+# p array2.reject
+# p array2.reject { |x| x < 6 }
 
+# p array2
+
+# puts 'my_reject array2'
+# p array2.my_reject
+# p array2.my_reject { |x| x < 6 }
+# puts "\n"
 # puts 'min array1'
 # p array1.my_min
 # puts "\n"
 
-# puts 'max array1'
-# p array1.my_max
-# puts "\n"
+puts 'max array2'
+p array2.max
+p array2.max(3)
+p array2.max { |a, b| -a <=> -b }
+p array2.max(2) { |a, b| -a <=> -b }
+puts "\n"
+
+puts 'my_max array2'
+p array2.my_max
+p array2.my_max(3)
+p array2.my_max { |a, b| -a <=> -b }
+p array2.my_max(2) { |a, b| -a <=> -b }
--- my_enumerable.rb
+++ my_enumerable.rb
@@ -9,21 +9,13 @@ module MyEnumerable
   def my_all?(pattern = nil)
     # all? [{|obj| block} ] → true or false
     # all?(pattern) → true or false
-    case
-    when block_given? && pattern.nil?   # {}
-      each do |item|
-        return false unless yield(item)
-      end
-    when !pattern.nil? && !block_given? # ()
-      each do |item|
-        return false unless pattern === item
-      end
-    when pattern.nil? && !block_given?  # empty
-      each do |item|
-        return false unless item
-      end
-    else # () {}
-      raise ArgumentError, 'Expected one parameter: either pattern, either block'
+    warn("given block not used\n", uplevel: 1) if !pattern.nil? && block_given?
+    if !pattern.nil? # ()
+      each { |item| return false unless pattern === item }
+    elsif block_given? # {}
+      each { |item| return false unless yield(item) }
+    else # empty
+      each { |item| return false unless item }
     end
     true
   end
@@ -31,21 +23,14 @@ module MyEnumerable
   def my_any?(pattern = nil)
     # any? [{|obj| block} ] → true or false
     # any?(pattern) → true or false
-    case
-    when block_given? && pattern.nil?   # {}
-      each do |item|
-        return true if yield(item)
-      end
-    when !pattern.nil? && !block_given? # ()
-      each do |item|
-        return true if pattern === item
-      end
-    when pattern.nil? && !block_given?  # empty
-      each do |item|
-        return true if item
-      end
-    else # () {}
-      raise ArgumentError, 'Expected one parameter: either pattern, either block'
+    warn("given block not used\n", uplevel: 1) if !pattern.nil? && block_given?
+
+    if !pattern.nil? # ()
+      each { |item| return true if pattern === item }
+    elsif block_given? # {}
+      each { |item| return true if yield(item) }
+    else # empty
+      each { |item| return true if item }
     end
     false
   end
@@ -53,57 +38,44 @@ module MyEnumerable
   def my_none?(pattern = nil)
     # none? [{|obj| block} ] → true or false
     # none?(pattern) → true or false
-    case
-    when block_given? && pattern.nil?   # {}
-      each do |item|
-        return false if yield(item)
-      end
-    when !pattern.nil? && !block_given? # ()
-      each do |item|
-        return false if pattern === item
-      end
-    when pattern.nil? && !block_given?  # empty
-      each do |item|
-        return false if item
-      end
-    else # () {}
-      raise ArgumentError, 'Expected one parameter: either pattern, either block'
+    warn("given block not used\n", uplevel: 1) if !pattern.nil? && block_given?
+
+    if !pattern.nil? # ()
+      each { |item| return false if pattern === item }
+    elsif block_given? # {}
+      each { |item| return false if yield(item) }
+    else # empty
+      each { |item| return false if item }
     end
     true
   end
 
-  def my_each
+  def my_each(&block)
     # each {|item| block} → ary
     # each → Enumerator
-    result_arr = []
-    if block_given?                   # {}
-      each do |item|
-        result_arr << yield(item)
-      end
-    elsif !block_given?               # empty
-      return to_enum(:each)
-    else
-      raise ArgumentError, 'Expected one parameter: block'
-    end
-    result_arr
+    return to_enum(:my_each) unless block_given?
+
+    each(&block)
+    self
   end
 
-  def my_include?(arr)
+  def my_include?(obj)
     # include?(object) → true or false
-    to_set.superset?(arr.to_set)
+    each { |item| return true if item == obj }
+    false
   end
 
   def my_map!
     # map! {|item| block } → ary
     # map! → Enumerator
-    result_arr = []
-    if block_given? # {}
-      each do |item|
-        result_arr << yield(item)
-      end
-    else            # empty
-      to_enum(:each)
+    return to_enum(:my_map!) unless block_given?
+
+    i = 0
+    while i < size
+      self[i] = yield(self[i])
+      i += 1
     end
+    self
   end
 
   def my_size
@@ -115,18 +87,14 @@ module MyEnumerable
     # count → int
     # count(obj) → int
     # count {|item| block} → int
-    if block_given? && value.nil?      # {}
-      each do |item|
-        count += 1 if yield(item)
-      end
-    elsif !value.nil? && !block_given? # ()
-      each do |item|
-        count += 1 if value == item
-      end
-    elsif value.nil? && !block_given?  # empty
+    warn("given block not used\n", uplevel: 1) if !value.nil? && block_given?
+    count = 0
+    if !value.nil? # ()
+      each { |item| count += 1 if value == item }
+    elsif block_given? # {}
+      each { |item| count += 1 if yield(item) }
+    else # empty
       return size
-    else
-      raise ArgumentError, 'Expected one parameter: either object, either block'
     end
     count
   end
@@ -139,66 +107,37 @@ module MyEnumerable
   def my_select
     # select {|item| block} → new_ary
     # select → Enumerator
-    result = []
-    if block_given? # {}
-      each do |item|
-        result << item if yield(item)
-      end
-    else            # empty
-      return to_enum(:each)
-    end
-    result
+    return to_enum(:my_select) unless block_given?
+
+    map { |item| item if yield(item) } - [nil]
   end
 
-  # def my_find(ifnone = nil)
-  #   # enum.find(ifnone = nil)   {| obj | block }  => obj or nil
-  #   if !value.nil?
-  #     while i < size
-  #       return self[i] if self[i] == value
+  def my_find(ifnone = nil)
+    # enum.find(ifnone = nil)   {| obj | block }  => obj or nil
+    return to_enum(:my_find, ifnone) unless block_given?
 
-  #       i += 1
-  #     end
-  #   elsif block_given?
-  #     while i < size
-  #       return self[i] if yield(self[i])
-
-  #       i += 1
-  #     end
-  #   else
-  #     p 'No condition given'
-  #   end
-  # end
+    each { |item| return item if yield(item) }
+    ifnone.nil? ? nil : ifnone.call
+  end
 
   def my_find_all
     # enum.find_all {| obj | block }  => array
-    result = []
-    if block_given? # {}
-      each do |item|
-        result << item if yield(item)
-      end
-    else            # empty
-      raise ArgumentError, 'Expected one parameter: block'
-    end
-    result
+    return to_enum(:my_find_all) unless block_given?
+
+    map { |item| item if yield(item) } - [nil]
   end
 
   def my_find_index(obj = nil)
     # find_index(obj) → int or nil
     # find_index {|item| block} → int or nil
     # find_index → Enumerator
-    index = nil
-    if !obj.nil? && !block_given?   # ()
-      each_with_index do |item, i|
-        return i if obj == item
-      end
-    elsif block_given? && obj.nil?  # {}
-      each_with_index do |item, i|
-        return i if yield(item)
-      end
-    elsif !block_given? && obj.nil? # empty
-      return to_enum(:each)
-    else
-      raise ArgumentError, 'Expected one parameter: either obj, either block'
+    warn("given block not used\n", uplevel: 1) if !obj.nil? && block_given?
+    if !obj.nil? # ()
+      (1..size).each { |i| return i if self[i] == obj }
+    elsif block_given? # {}
+      (1..size).each { |i| return i if yield(self[i]) }
+    else # empty
+      return to_enum(:my_find_index)
     end
     nil
   end
@@ -206,44 +145,56 @@ module MyEnumerable
   def my_reject
     # reject {|item| block } → new_ary
     # reject → Enumerator
-    result = self
-    if block_given? # {}
-      each_with_index do |item, i|
-        result.delete_at(i) if yield(item)
-      end
-    else            # empty
-      return to_enum(:each)
-    end
-    result
+    return to_enum(:my_reject) unless block_given?
+
+    map { |item| item unless yield(item) } - [nil]
   end
 
-  # def my_min(n = nil)
-  #   # min → obj
-  #   # min {| a,b | block } → obj
-  #   # min(n) → array
-  #   # min(n) {| a,b | block } → array
-  #   min_el = self[0]
-  #   min_array = []
-  #   if n.nil? && !block_given?
-  #     each do |item|
-  #       min_el = item if min_el > item
+  def my_min(amount = nil, &block)
+    # min → obj
+    # min {| a,b | block } → obj
+    # min(n) → array
+    # min(n) {| a,b | block } → array
+    if amount.nil?
+      min_el = self[0]
+      if block_given?
+        each { |item| min_el = item if yield(item, min_el).negaite? }
+      else
+        each { |item| min_el = item if item < min_el }
+      end
+      min_el
+    else
+      arr = []
+      (arr << self).flatten!
+      (1..amount).map do
+        tmp = arr.my_min(&block)
+        arr.delete(tmp)
+      end
+    end
+  end
 
-  #     end
-  #   elsif n.nil? && block_given?
-
-  #   min_el
-  # end
-
-  # def my_max
-  #   max_el = self[0]
-  #   i = 1
-  #   while i < size
-  #     max_el = self[i] if min < self[i]
-
-  #     i += 1
-  #   end
-  #   max_el
-  # end
+  def my_max(amount = nil, &block)
+    # min → obj
+    # min {| a,b | block } → obj
+    # min(n) → array
+    # min(n) {| a,b | block } → array
+    if amount.nil?
+      max_el = self[0]
+      if block_given?
+        each { |item| max_el = item if yield(item, max_el).positive? }
+      else
+        each { |item| max_el = item if item > max_el }
+      end
+      max_el
+    else
+      arr = []
+      (arr << self).flatten!
+      (1..amount).map do
+        tmp = arr.my_max(&block)
+        arr.delete(tmp)
+      end
+    end
+  end
 end
 
 class Array
