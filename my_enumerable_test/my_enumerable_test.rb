86ea42f34cf7b68228b902a6b4ff6e238e33df09
knooki <knukknakiknak@gmail.com>
2022-10-16 20:17:50 +0300

    added files for help

--- my_enumerable_test/my_enumerable_test.rb
+++ my_enumerable_test/my_enumerable_test.rb
@@ -1,6 +1,9 @@
 require_relative '../my_enumerable'
 
 RSpec.describe MyEnumerable do
-  let(:arr) { [1, 2, 3, 4] }
-  
+  let(:arr) { [1, 2, 3, 4, 5, 6] }
+
+  describe 'my_all?' do
+    context ''
+  end
 end
